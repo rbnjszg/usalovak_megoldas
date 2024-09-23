b92f4914fa4a20ec7caafe4652d41b7976aab50d
rbnjszg <rakitovszky.balint@njszg.hu>
2024-09-23 18:38:56 +0000

    Útvonalak

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HorseController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [HorseController::class, "index"])->name("home");
+Route::get("horse/list", [HorseController::class, "list"])->name("horse.list");
+Route::get("horse/table", [HorseController::class, "table"])->name("horse.table");
+Route::get("horse/grid", [HorseController::class, "grid"])->name("horse.grid");
